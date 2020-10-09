60e663380333164e71545238e72e1ab91e06f8bb
rakanishuman <dannynguyen@hotmail.ca>
2020-10-09 09:42:11 -0600

    First Commit

---
+++ ResumeTakeTwo.js
@@ -0,0 +1,21 @@
+function getInfo(info, button) {
+  var x = document.getElementById(info);
+  var y = document.getElementsByClassName('navCard');
+  var z = document.getElementById(button);
+  x.className += " w3-show";
+  for (i = 0; i < y.length; i++) {
+    y[i].className += " w3-hide";
+  }
+  document.getElementById('backButton').className += ' w3-show';
+}
+function hideInfo() {
+  var x = document.getElementsByClassName('info');
+  var y = document.getElementsByClassName('navCard');
+  for (j = 0; j < x.length; j++) {
+    x[j].className = x[j].className.replace(" w3-show", "");
+  }
+  for (i = 0; i < y.length; i++) {
+    y[i].className = y[i].className.replace(" w3-hide", "");
+  }
+  document.getElementById('backButton').className = document.getElementById('backButton').className.replace(" w3-show", "");
+}
