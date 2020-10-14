53cd960a00ba71a1862e62e733e9c810e118d4a0
rakanishuman <dannynguyen@hotmail.ca>
2020-10-14 10:57:55 -0600

    90% complete. Will find more stuff to add.

--- ResumeTakeTwo.js
+++ ResumeTakeTwo.js
@@ -1,10 +1,8 @@
-function getInfo(info, button) {
-  var x = document.getElementById(info);
-  var y = document.getElementsByClassName('navCard');
-  var z = document.getElementById(button);
-  x.className += " w3-show";
-  for (i = 0; i < y.length; i++) {
-    y[i].className += " w3-hide";
+function getInfo(info) {
+  var x = document.getElementsByClassName('navCard');
+  document.getElementById(info).className += " w3-show";
+  for (y = 0; y < x.length; y++) {
+    x[y].className += " w3-hide";
   }
   document.getElementById('backButton').className += ' w3-show';
 }
@@ -19,4 +17,9 @@ function hideInfo() {
   }
   document.getElementById('backButton').className = document.getElementById('backButton').className.replace(" w3-show", "");
 }
-
+function hoverStart(button){
+  document.getElementById(button).style.backgroundColor='#FBA90A'
+}
+function hoverEnd(button){
+  document.getElementById(button).style.backgroundColor='#B6A9F0';
+}
